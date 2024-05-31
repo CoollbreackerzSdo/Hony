6986db67a25e1dd616c194f58c85778ad9b19468
Coollbreackerz <ralberinfante4@gmail.com>
2024-05-31 09:20:54 -0400

    create migration v1

--- src/Hony.Api/Program.cs
+++ src/Hony.Api/Program.cs
@@ -1,13 +1,19 @@
 using Hony.Api;
+using Hony.Infrastructure;
+using Hony.Infrastructure.Database;
 
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvConfigurations();
+builder.Services.AddInfrastructure();
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+using var scopeContext = app.Services.CreateScope().ServiceProvider.GetRequiredService<HonyNpSqlContext>();
+scopeContext.Database.Migrate();
 
 app.MapOpenApi();
 app.MapScalarApiReference();
@@ -16,7 +22,8 @@ app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/home", () => new {
+app.MapGet("/home", () => new
+{
     Title = "Coca Cola",
     Name = "Mons"
 })
--- src/Hony.Infrastructure/ExtensionsBuilderServices.cs
+++ src/Hony.Infrastructure/ExtensionsBuilderServices.cs
@@ -1,6 +1,17 @@
-﻿namespace Hony.Infrastructure;
+﻿using Hony.Infrastructure.Database;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hony.Infrastructure;
 
 public static class ExtensionsBuilderServices
 {
-
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddDbContext<HonyNpSqlContext>(config =>
+        {
+            config.UseNpgsql(Environment.GetEnvironmentVariable("POST_CONNECTION"), b => b.MigrationsAssembly("Hony.Api"));
+        });
+        return services;
+    }
 }

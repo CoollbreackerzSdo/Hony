cf8ab85e4891ff1d1f5f704a40b03743b8615811
Coollbreackerz <ralberinfante4@gmail.com>
2024-05-30 22:31:39 -0400

    add test projects

---
+++ src/Hony.Api/Globals.cs
@@ -0,0 +1,3 @@
+global using Ardalis.Result;
+global using Microsoft.EntityFrameworkCore;
+global using MongoDB.Driver;
---
+++ test/Hony.Test.NUnit/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Hony.Test.NUnit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

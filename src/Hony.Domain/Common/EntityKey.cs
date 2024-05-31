f757419aa9dbdb59f7d7cc1aa0e091e624ce5da7
Coollbreackerz <ralberinfante4@gmail.com>
2024-05-31 07:02:29 -0400

    create infra design

--- src/Hony.Domain/Common/EntityKey.cs
+++ src/Hony.Domain/Common/EntityKey.cs
@@ -1,11 +1,11 @@
 namespace Hony.Domain.Common;
 
-public readonly struct EntityKey<T>(T Value) : IComparable<EntityKey<T>>
+public record struct EntityKey<T>(T value) : IComparable<EntityKey<T>>
     where T : notnull, IComparable<T>
 {
     public readonly int CompareTo(EntityKey<T> other)
         => other is EntityKey<T> model ? Value.CompareTo(model.Value) : 0;
-    public T Value { get; init; } = Value;
+    public T Value { get; init; } = value;
     public static implicit operator EntityKey<T>(T value)
         => new(value);
     public static implicit operator T(EntityKey<T> value)
